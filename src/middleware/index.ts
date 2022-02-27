62932502ce35dadee7553b19eafaa24cecb3c4c8
seho <1018715564@qq.com>
2022-02-27 22:35:27 +0800

    ✨ feat: 新增中间件
    usePostApiCallPipeline usePreApiCallPipeline

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,26 +1,34 @@
 import type { PipelineNode, PipelineNodeReturn } from '../../typings/middleware/index';
 
 const pipeline: PipelineNode<any>[] = [];
+const preApiCallPipeline: PipelineNode<any>[] = [];
+const postApiCallPipeline: PipelineNode<any>[] = [];
 
-const push = <T>(cb: PipelineNode<T>) => {
-  pipeline.push(cb);
-  return cb;
-};
+// 推送到队列中
+const push =
+  <T>(cb: PipelineNode<T>) =>
+  (pipeline: PipelineNode<any>[]) => {
+    pipeline.push(cb);
+    return cb;
+  };
 
-const exec = async <T>(input: T) => {
-  let res = input;
-  for (let i = 0; i < pipeline.length; i++) {
-    try {
-      res = await pipeline[i](res);
-    } catch (error) {
-      throw new Error('pipeline eror');
+// 执行队列
+const exec =
+  <T>(input: T) =>
+  async (pipeline: PipelineNode<any>[]) => {
+    let res = input;
+    for (let i = 0; i < pipeline.length; i++) {
+      try {
+        res = await pipeline[i](res);
+      } catch (error) {
+        return new Error('pipeline eror');
+      }
+      if (res === undefined || res === null) {
+        return res;
+      }
     }
-    if (res === undefined || res === null) {
-      return res;
-    }
-  }
-  return res;
-};
+    return res;
+  };
 
 /**
  *
@@ -47,10 +55,88 @@ const exec = async <T>(input: T) => {
  */
 export const usePipeline = <T>(): {
   push: <K extends T>(cb: PipelineNode<K>) => PipelineNode<K>;
-  exec: <K extends T>(input: K) => Promise<PipelineNodeReturn<K>>;
+  exec: <K extends T>(input: K) => Promise<PipelineNodeReturn<K | Error>>;
 } => {
   return {
-    push,
-    exec
+    push: (cb) => {
+      return push(cb)(pipeline);
+    },
+    exec: (input) => {
+      return exec(input)(pipeline);
+    }
+  };
+};
+
+/**
+ *
+ * @name 使用PreApiCall管道
+ * @description
+ * 可以给`usePreApiCallPipeline`传递一个类型T，代表了管道流通的数据类型，
+ * 在`push`这个方法中也可以传递一个T的子类型，参数为一个可异步也可同步的cb，
+ * 而`exec`方法则就是执行队列中存储的cb，如果遇到null|undefined则就会立即停止
+ * 执行。
+ * @example
+ *  const pipeline = usePreApiCallPipeline();
+ *  pipeline.push(() => {
+ *     console.log("管道1")
+ *  })
+ *  pipeline.push(async () => {
+ *     console.log("管道2")
+ *  })
+ *  pipeline.exec({default: "message"})
+ * @template T
+ * @return {*}  {{
+ *   push: <K extends T>(cb: PipelineNode<K>) => void;
+ *   exec: <K extends T>(input: K) => Promise<PipelineNodeReturn<K>>;
+ * }}
+ */
+export const usePreApiCallPipeline = <T>(): {
+  push: <K extends T>(cb: PipelineNode<K>) => PipelineNode<K>;
+  exec: <K extends T>(input: K) => Promise<PipelineNodeReturn<K | Error>>;
+} => {
+  return {
+    push: (cb) => {
+      return push(cb)(preApiCallPipeline);
+    },
+    exec: (input) => {
+      return exec(input)(preApiCallPipeline);
+    }
+  };
+};
+
+/**
+ *
+ * @name 使用PostApiCall管道
+ * @description
+ * 可以给`usePostApiCallPipeline`传递一个类型T，代表了管道流通的数据类型，
+ * 在`push`这个方法中也可以传递一个T的子类型，参数为一个可异步也可同步的cb，
+ * 而`exec`方法则就是执行队列中存储的cb，如果遇到null|undefined则就会立即停止
+ * 执行。
+ * @example
+ *  const pipeline = usePostApiCallPipeline();
+ *  pipeline.push(() => {
+ *     console.log("管道1")
+ *  })
+ *  pipeline.push(async () => {
+ *     console.log("管道2")
+ *  })
+ *  pipeline.exec({default: "message"})
+ * @template T
+ * @return {*}  {{
+ *   push: <K extends T>(cb: PipelineNode<K>) => void;
+ *   exec: <K extends T>(input: K) => Promise<PipelineNodeReturn<K>>;
+ * }}
+ */
+export const usePostApiCallPipeline = <T>(): {
+  push: <K extends T>(cb: PipelineNode<K>) => PipelineNode<K>;
+  exec: <K extends T>(input: K) => Promise<PipelineNodeReturn<K | Error>>;
+} => {
+  return {
+    push: (cb) => {
+      return push(cb)(postApiCallPipeline);
+    },
+    exec: (input) => {
+      return exec(input)(postApiCallPipeline);
+    }
   };
 };
--- tests/middleware/pipeline.test.ts
+++ tests/middleware/pipeline.test.ts
@@ -1,11 +1,67 @@
-import { usePipeline } from '../../src';
+import { usePipeline, usePostApiCallPipeline, usePreApiCallPipeline } from '../../src';
 
-describe('管道测试', () => {
+describe('usePipeline', () => {
   it('同步和异步方法混合', () => {
     type Msg = {
       data: string
     }
     const pipeline = usePipeline<Msg>();
+    const defaultData: Msg = {
+      data: "default"
+    }
+    pipeline.push((e: Msg) => {
+      expect(e).toBe({data: "default"});
+      return {
+        data: '我是节点1'
+      };
+    });
+
+    pipeline.push((e: Msg) => {
+      expect(e).toBe({data: "我是节点1"});
+      return {
+        data: '我是节点2'
+      };
+    });
+
+    pipeline.exec(defaultData)
+  });
+});
+
+
+describe('usePostApiCallPipeline', () => {
+  it('同步和异步方法混合', () => {
+    type Msg = {
+      data: string
+    }
+    const pipeline = usePostApiCallPipeline<Msg>();
+    const defaultData: Msg = {
+      data: "default"
+    }
+    pipeline.push((e: Msg) => {
+      expect(e).toBe({data: "default"});
+      return {
+        data: '我是节点1'
+      };
+    });
+
+    pipeline.push((e: Msg) => {
+      expect(e).toBe({data: "我是节点1"});
+      return {
+        data: '我是节点2'
+      };
+    });
+
+    pipeline.exec(defaultData)
+  });
+});
+
+
+describe('usePreApiCallPipeline', () => {
+  it('同步和异步方法混合', () => {
+    type Msg = {
+      data: string
+    }
+    const pipeline = usePreApiCallPipeline<Msg>();
     const defaultData: Msg = {
       data: "default"
     }
