f3ab4ad9df4a398022c06d0506c8f96176b61cd4
seho <1018715564@qq.com>
2022-03-14 11:29:36 +0800

    🎈 perf: 优化protoschema读取方式

--- src/core/api.ts
+++ src/core/api.ts
@@ -1,27 +1,23 @@
 import { useQuery, useBody, sendError } from 'h3';
-import { TSBufferProtoGenerator } from 'tsbuffer-proto-generator';
-import { getApiProtoMap } from './map';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { getApiMap } from './map';
 import log from '../log';
 import { validateMethod, validateProto, getNeedValidateProto } from './validate';
 import error from './error';
 import type { App } from 'h3';
 import type { HttpContext } from '@sword-code-practice/types/sword-backend-framework';
 
-// api对应的proto.ts的字典
-let protoMap: Record<string, string> | null = null;
 // 具体的proto schema引用
 let protoSchema: Record<string, Record<string, unknown>> | null = null;
 
 export const implementApi = async (app: App, dirName: string) => {
   // 获取apimap以及proto map
-  const { apiMap, protoMap: _protoMap } = await getApiProtoMap(dirName);
-  // 将protomap赋值给全局变量，再编译api资源成功后，会去编译proto.json
-  protoMap = _protoMap;
+  const { apiMap } = await getApiMap(dirName);
+  // 获取proto schema
   getProtoSchema();
   for (const key in apiMap) {
     // key: api value: path
-    // 清除缓存，让旧模块无效，无需重启服务器，在开发阶段享受HMR
-    delete require.cache[apiMap[key].sourcePath];
     app?.use(key, async (req, res) => {
       // 打印json日志
       log.info(`[请求URL]: ${key}`);
@@ -92,14 +88,10 @@ const createContext = (context: Partial<HttpContext>): HttpContext => {
   };
 };
 
-// 通过proto map 生成 proto json类型
+// 加载根目录下的protoschema（由cli生成）
 const getProtoSchema = async () => {
-  // 迭代protoMap，异步执行生成类型的函数
-  const result: Record<string, Record<string, unknown>> = {};
-  for (const key in protoMap) {
-    const generator = new TSBufferProtoGenerator();
-    result[key] = await generator.generate(protoMap[key]);
+  const schema = readFileSync(resolve(process.cwd(), './src/proto.json')).toString();
+  if (schema) {
+    protoSchema = JSON.parse(schema) as any;
   }
-  // 将result放入到schema引用中
-  protoSchema = result;
 };
