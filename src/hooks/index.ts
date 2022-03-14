bfeee113be600b242d07b647812f7bef58ed7901
seho <1018715564@qq.com>
2022-03-14 11:31:20 +0800

    ✨ feat: h3服务器端口占用处理

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 import { createServer } from 'http';
+import net from 'net';
 import { createApp } from 'h3';
 import { implementApi } from '../core/api';
 import log from '../log';
@@ -17,16 +18,70 @@ export const useApp = () => {
   };
 };
 
+type ServerConfig = {
+  port?: number;
+};
+
+const defaultServerConfig: ServerConfig = {
+  port: 3000
+};
+
 // 启动服务器
-const startServer = () => {
-  log.info(`HTTP服务启动中...`);
-  const server = createServer(app);
-  try {
-    server.listen(3000);
-    log.success(`程序运行在3000端口上`);
-  } catch (error) {
-    log.err(JSON.stringify(error));
+const startServer = async (serverConfig: ServerConfig = defaultServerConfig) => {
+  let key: keyof ServerConfig;
+  for (key in serverConfig) {
+    if (!serverConfig[key]) {
+      serverConfig[key] = defaultServerConfig[key];
+    }
   }
+
+  // 启动服务器
+  const start = () => {
+    log.info(`HTTP服务启动中...`);
+    const server = createServer(app);
+    try {
+      // 判断
+      server.listen(serverConfig.port);
+      log.success(`程序运行在${serverConfig.port}端口上`);
+    } catch (error) {
+      log.err(JSON.stringify(error));
+    }
+  };
+
+  // 尝试检测port被占用情况
+  const tryUsePort = async (): Promise<any> => {
+    // 查看端口是否占用
+    const used = await portUsed(serverConfig.port as number);
+    if (used === false) {
+      // 如果端口被占用，就把port++
+      serverConfig.port && serverConfig.port++;
+      return await tryUsePort();
+    } else {
+      // 没有被占用返回true
+      return true;
+    }
+  };
+
+  if (await tryUsePort()) {
+    // port合法
+    start();
+  }
+};
+
+// 端口是否使用
+const portUsed = (port: number): Promise<number | false> => {
+  return new Promise((resolve) => {
+    const server = net.createServer().listen(port);
+    server.on('listening', function () {
+      server.close();
+      resolve(port);
+    });
+    server.on('error', function (err: any) {
+      if (err.code == 'EADDRINUSE') {
+        resolve(false);
+      }
+    });
+  });
 };
 
 export * from './instruct';
