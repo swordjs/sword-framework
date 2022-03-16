ff98fbf1dcffad54180d8dccd0624f3d0e37342f
seho <1018715564@qq.com>
2022-03-16 23:39:29 +0800

    🎈 perf: 优化middleware pipeline

--- tests/middleware/pipeline.test.ts
+++ tests/middleware/pipeline.test.ts
@@ -1,53 +1,46 @@
-import { usePostApiCallPipeline, usePreApiCallPipeline, exec, preApiCallPipeline, postApiCallPipeline } from '../../src/middleware';
+import { usePipeline, exec } from '../../src/middleware';
+
+const pipeline = usePipeline<any>();
 
 describe('usePostApiCallPipeline', () => {
   it('同步和异步方法混合', () => {
-    type Msg = {
-      data: string;
-    };
-    const pipeline = usePostApiCallPipeline<Msg>();
-    const defaultData: Msg = {
+    const defaultData: any = {
       data: 'default'
     };
-    pipeline.push((e: Msg) => {
+    pipeline('postApiCall', (e: any) => {
       expect(e).toBe({ data: 'default' });
       return {
         data: '我是节点1'
       };
     });
-
-    pipeline.push((e: Msg) => {
+    pipeline('postApiCall', (e: any) => {
       expect(e).toBe({ data: '我是节点1' });
       return {
         data: '我是节点2'
       };
     });
-    exec(defaultData)(postApiCallPipeline);
+    exec<any>('postApiCall', defaultData);
   });
 });
 
 describe('usePreApiCallPipeline', () => {
   it('同步和异步方法混合', () => {
-    type Msg = {
-      data: string;
-    };
-    const pipeline = usePreApiCallPipeline<Msg>();
-    const defaultData: Msg = {
+    const defaultData = {
       data: 'default'
     };
-    pipeline.push((e: Msg) => {
+    pipeline('preApiCall', (e: any) => {
       expect(e).toBe({ data: 'default' });
       return {
         data: '我是节点1'
       };
     });
 
-    pipeline.push((e: Msg) => {
+    pipeline('preApiCall', (e: any) => {
       expect(e).toBe({ data: '我是节点1' });
       return {
         data: '我是节点2'
       };
     });
-    exec(defaultData)(preApiCallPipeline);
+    exec<any>('preApiCall', defaultData);
   });
 });
