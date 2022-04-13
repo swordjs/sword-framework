2e80f3d5487d8d54782102e2e9c6c4006fbc8270
seho <seho.zaq@gmail.com>
2022-04-13 23:42:43 +0800

    🎈 perf: 完善检索目录生成资源的功能

--- packages/cli/tests/config/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { getConfig } from '../../src/config';
-
-describe('获取配置对象', () => {
-  it('1', async () => {
-    console.log(1);
-    const config = await getConfig();
-    console.log(config);
-  });
-});
