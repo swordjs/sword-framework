181623d6a825fde97476e080db3e9685dcefd517
seho <seho.zaq@gmail.com>
2022-04-13 23:39:43 +0800

    🎈 perf: 完善config注入

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -1,12 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
 import { loadConfig } from 'unconfig';
 import type { Config } from '../typings/config';
 
-// 默认配置的类型是非必选的config
-const defaultConfig: Required<Config> = {};
+export let configData: Config;
 
-export const getConfig = async () => {
+// 默认配置的类型是非必选的config
+const defaultConfig: Required<Config> = {
+  platform: 'server'
+};
+
+export const initConfig = async () => {
   const { config } = await loadConfig<Config>({
     sources: [
       {
@@ -14,27 +16,25 @@ export const getConfig = async () => {
         // default extensions
         extensions: ['ts', 'mts', 'cts', 'js', 'mjs', 'cjs', 'json']
       },
-      // load `my` field in `package.json` if no above config files found
       {
         files: 'package.json',
         extensions: [],
-        rewrite(config) {
+        rewrite(config: any) {
           return config?.sword;
         }
       }
     ],
-    // if false, the only the first matched will be loaded
-    // if true, all matched will be loaded and deep merged
     merge: false
   });
   if (typeof config === 'undefined') return defaultConfig;
   // 遍历defaultConfig，如果config没有传入某个配置，则使用defaultConfig中的内容
-  for (const key in defaultConfig) {
+  let key: keyof typeof defaultConfig;
+  for (key in defaultConfig) {
     if (!config[key]) {
       config[key] = defaultConfig[key];
     }
   }
-  return config as Required<Config>;
+  configData = config;
 };
 
 export type { Config } from '../typings/config';
