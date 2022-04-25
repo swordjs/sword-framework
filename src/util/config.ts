b4d689ed48d57d47cf9db76ddcce1b08feac482d
seho <seho.zaq@gmail.com>
2022-04-25 22:12:38 +0800

    🐞 fix: 修复process.argv丢失的bug

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,11 +1,18 @@
 import mri from 'mri';
+import type { Argv } from 'mri';
 import type { CommandConfig } from '../../typings/config';
 
+let argv: any = null;
+
 // 用于解析命令行参数, 附带一个默认command默认对象
-export const parseCommandArgs = () => {
-  return mri<CommandConfig>(process.argv.splice(2), {
+export const parseCommandArgs = (): Argv<CommandConfig> => {
+  if (argv) {
+    return argv;
+  }
+  argv = mri<CommandConfig>(process.argv.slice(2), {
     default: {
       platform: 'server'
     }
   });
+  return argv;
 };
