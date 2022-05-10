b7439f2175e4ba360ff2fa7c8afc2c5c374d9f9a
seho <seho.zaq@gmail.com>
2022-05-10 23:46:13 +0800

    ✨ feat: add unicloud logger

--- packages/faas-uni/src/index.ts
+++ packages/faas-uni/src/index.ts
@@ -1,3 +1,24 @@
-export const main = () => {
+import type { UnicloudContext, UnicloudEvent } from '../../../typings/unicloud';
+import type { Logger } from '../../../typings/log';
+import type { Map } from '../../../src/core/map';
+
+/**
+ * unicloud 平台触发api
+ * @param {UnicloudEvent} event
+ * @param {UnicloudContext} context
+ * @return {*}
+ */
+export const triggerApi = (event: UnicloudEvent, context: UnicloudContext, apiMap: Map) => {
+  console.log(apiMap);
   return '';
+};
+
+export const logger: Logger = {
+  REQUEST_URL: (key: string) => console.log(`[请求URL]: ${key}`),
+  REQUEST_METHOD_ERROR: (msg: string) => console.log(`[请求方法错误]: ${msg}`),
+  REQUEST_TYPE_ERROR: (msg: string) => console.log(`[请求类型校验错误]:${msg}`),
+  REQUEST_QUERY: (query: string) => console.log(`[请求参数-query]: ${query}`),
+  REQUEST_PARAMS: (query: string) => console.log(`[请求参数-params]: ${query}`),
+  RESPONSE_RESULT: (msg: string, suffix = '') => console.log(`[返回结果${suffix}]: ${msg}`),
+  RESPONSE_TYPE_ERROR: (msg: string) => console.log(`[返回类型校验错误]:${msg}`)
 };
