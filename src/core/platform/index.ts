ffa3f0467ac5dcf204483a944ea934a27b5db34d
seho <seho.zaq@gmail.com>
2022-06-02 17:48:38 +0800

    🎈 perf: update platformhook params

--- src/core/platform/index.ts
+++ src/core/platform/index.ts
@@ -1,15 +1,16 @@
 import { commandArgs } from '../../util/config';
+
 /**
  * 根据不同平台, 传入不同的函数, 并且返回
  * @param {(Record<typeof commandArgs.platform, () => Promise<any> | any>)} params
  * @return {*}
  */
 export const platformHook = async <R = any>(
-  params: Record<typeof commandArgs.platform, () => Promise<R> | R>,
+  params: Partial<Record<typeof commandArgs.platform, () => Promise<R> | R>>,
   platform = commandArgs.platform
 ): Promise<R | undefined> => {
   // 判断存在且是函数
   if (params[platform] && typeof params[platform] === 'function') {
-    return await params[platform]();
+    return await (params[platform] as () => R)();
   }
 };
