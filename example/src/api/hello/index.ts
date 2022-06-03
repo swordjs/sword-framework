c56d7a536af8af52eb529f8499166b42311ea416
seho <seho.zaq@gmail.com>
2022-06-03 21:52:27 +0800

    🎈 perf: update export default

--- example/src/api/hello/index.ts
+++ example/src/api/hello/index.ts
@@ -1,7 +1,7 @@
 import { useApi } from '@sword-code-practice/sword-framework';
 import { ReqQuery, ReqParams, Res } from './proto';
 
-export const main = useApi<{
+export default useApi<{
   query: ReqQuery;
   params: ReqParams;
   res: Res;
