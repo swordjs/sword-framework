d3fb4d9050455ac1d023e23e01d32d856c8a8cf3
seho <seho.zaq@gmail.com>
2022-06-10 15:53:06 +0800

    🎈 perf: add platformhook default option

--- packages/runtime/src/core/platform/index.ts
+++ packages/runtime/src/core/platform/index.ts
@@ -1,16 +1,22 @@
 import { commandArgs } from '../../../../../util/config';
 
 /**
+ *
  * 根据不同平台, 传入不同的函数, 并且返回
- * @param {(Record<typeof commandArgs.platform, () => Promise<any> | any>)} params
- * @return {*}
+ * @template R
+ * @param {(Partial<Record<typeof commandArgs.platform | 'default', () => Promise<R> | R>>)} params
+ * @param {*} [platform=commandArgs.platform]
+ * @return {*}  {(Promise<R | undefined>)}
  */
 export const platformHook = async <R = any>(
-  params: Partial<Record<typeof commandArgs.platform, () => Promise<R> | R>>,
+  params: Partial<Record<typeof commandArgs.platform | 'default', () => Promise<R> | R>>,
   platform = commandArgs.platform
 ): Promise<R | undefined> => {
   // 判断存在且是函数
   if (params[platform] && typeof params[platform] === 'function') {
     return await (params[platform] as () => R)();
+  } else if (params['default'] && typeof params['default'] === 'function') {
+    // 如果不存在，则默认default函数返回值
+    return await (params['default'] as () => R)();
   }
 };
