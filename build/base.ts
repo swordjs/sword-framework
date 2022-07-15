4d2e1d3ef2132023bc8bfc71f3f2b1fb404cd00c
seho <seho.zaq@gmail.com>
2022-07-15 23:07:15 +0800

    🎈 perf: add base build config

---
+++ build/base.ts
@@ -0,0 +1,26 @@
+import { defineBuildConfig } from 'unbuild';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+const paths = JSON.parse(readFileSync(resolve('../../tsconfig.json')).toString()).compilerOptions.paths;
+const alias: any = {};
+
+for (const key in paths) {
+  // 为package目录中的子包提供服务, 在子包运行命令时, 需要跳出2个文件夹找到根目录的tsconfig, 而不是子包的tsconfig
+  alias[key] = resolve('../../', paths[key][0].replace('/*', ''));
+}
+
+export default defineBuildConfig({
+  entries: ['./src/index'],
+  outDir: 'lib',
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+    // 从tsconfig.json读取paths
+    alias,
+    cjsBridge: true,
+    esbuild: {
+      target: 'node12'
+    }
+  }
+});
