76840ece661f3da65568f5ce07f2de3e328175b0
seho <seho.zaq@gmail.com>
2022-07-16 19:12:57 +0800

    🔧 build: add pnpm build scripts

--- scripts/build-packages.mjs
+++ scripts/build-packages.mjs
@@ -1,20 +1,11 @@
 import { spawn } from 'child_process';
-import { readdirSync } from 'fs';
-import { resolve } from 'path';
 
-// 遍历packages
-const packages = readdirSync(resolve('packages')).filter((p) => !['.DS_Store', 'README.md'].includes(p));
-
-packages.forEach((pkg) => {
-  // 运行build命令
-  // cd 子目录, 并且执行npm build
-  spawn('pnpm', ['build'], {
-    cwd: resolve('packages', pkg)
-  }).on('close', (code) => {
-    if (code !== 0) {
-      console.error(`${pkg} build failed`);
-    } else {
-      console.log(`${pkg} build success`);
-    }
-  });
+spawn('pnpm', ['--filter', './packages/**', 'build'], {
+  stdio: 'inherit'
+}).on('close', (code) => {
+  if (code !== 0) {
+    console.error(`build failed`);
+  } else {
+    console.log(`build success`);
+  }
 });
