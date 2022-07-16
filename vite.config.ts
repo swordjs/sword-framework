f10a489da1bfaa2621c4e3d051a0d39c98b0b28d
seho <seho.zaq@gmail.com>
2022-07-16 20:43:13 +0800

    🎈 perf: socket.io-client alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import alias from './util/alias';
 
 export default defineConfig({
   resolve: {
-    alias: alias('./tsconfig.json')
+    alias: {
+      ...alias('./tsconfig.json'),
+      'socket.io-client': 'socket.io-client/dist/socket.io.j'
+    }
   }
 });
