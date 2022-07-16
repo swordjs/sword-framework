deaf9bb18374790b752444e8da7d9f3ae0172374
seho <seho.zaq@gmail.com>
2022-07-16 18:46:01 +0800

    🎈 perf: abstract camelCase util to global

--- util/index.ts
+++ util/index.ts
@@ -20,3 +20,13 @@ export const throttle = (fn: (...params: any[]) => any, delay: number) => {
     }, delay);
   };
 };
+
+export const camelCase = (name: string) => {
+  const SPECIAL_CHARS_REGEXP = /([\:\-\_]+(.))/g;
+  const MOZ_HACK_REGEXP = /^moz([A-Z])/;
+  return name
+    .replace(SPECIAL_CHARS_REGEXP, function (_, separator, letter, offset) {
+      return offset ? letter.toUpperCase() : letter;
+    })
+    .replace(MOZ_HACK_REGEXP, 'Moz$1');
+};
