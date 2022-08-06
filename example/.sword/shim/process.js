695ccdb9ba6247f8b0f2d31c77d26914dbb408b4
seho <seho.zaq@gmail.com>
2022-08-06 21:42:12 +0800

    🎈 perf: remove .sword dir

--- example/.sword/shim/process.js
+++
@@ -1,5 +0,0 @@
-
-  // process shim
-  process.argv = ["/usr/local/bin/node","/Users/seho/Desktop/openSource/sword-function-framework/packages/cli/lib/index.cjs","dev"]
-  process.env._sword_command = dev
-  
