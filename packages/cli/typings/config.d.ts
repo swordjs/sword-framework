939e6b74450042e7fb1ec406df7342f8e5eb5d38
seho <seho.zaq@gmail.com>
2022-12-10 23:40:30 +0800

    perf: add new config type in `share`

--- packages/cli/typings/config.d.ts
+++ packages/cli/typings/config.d.ts
@@ -2,4 +2,17 @@ export interface Config {
   unicloud?: {
     link: string;
   };
+  // 共享目录
+  share?: {
+    // share目录的名称
+    dirName: string;
+    // share目录的路径
+    path?: string;
+    type?: {
+      dirName?: string;
+      package?: {
+        [key: string]: any
+      }
+    }
+  }
 }
