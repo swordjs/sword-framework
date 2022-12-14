1ae55cfcb9799244d8430981986ac7b76df53f01
seho <seho.zaq@gmail.com>
2022-12-15 00:41:56 +0800

    perf: add typescript on peerDependencies & typescript parser

--- packages/cli/src/share.ts
+++ packages/cli/src/share.ts
@@ -1,3 +1,4 @@
+import * as ts from "typescript";
 import { isDev } from '~util/env';
 import log from './log';
 import { readFileSync } from 'fs';
@@ -14,6 +15,19 @@ export default async (args: Argv<CommandConfig>) => {
             // 获取其中的protopath, 并且读取文件内容
             const protoPath = apiJson[key].protoPath;
             const protoContent = readFileSync(protoPath as string).toString();
+            // 判断用户是否安装了typescript
+            try {
+                require.resolve('typescript');
+            } catch (error) {
+                log.err(`You need to install typescript first, please run the following command:
+                npm install typescript -D
+                or
+                yarn add typescript -D`)
+                return;
+            }
+            // 拿到proto文件内容后, 使用ts官方的parser库, 对其内容进行ast分析
+            const sourceFile = ts.createSourceFile(protoPath as string, protoContent, ts.ScriptTarget.Latest, true);
+            console.log(sourceFile)
         }
     } else {
         log.err(`When you run the share command, the current directory must be in the dev environment. 
